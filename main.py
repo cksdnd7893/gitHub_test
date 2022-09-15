15b92539d95a69b1372b5eb91dba84018394a79d
Ungs <dkfnfn7893@naver.com>
2022-09-15 13:52:18 +0900

    second commit

--- main.py
+++ main.py
@@ -3,5 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+tmp = 1
 
-print("hello world")
+if(tmp == 1):
+    print("hello world")
